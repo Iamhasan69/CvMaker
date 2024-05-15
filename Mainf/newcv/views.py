8a2a8f044790b177dbb416ea6cff5f96fb59479d
Tufail Khan <112545445+tufailKhan-dev@users.noreply.github.com>
2024-05-15 14:48:50 +0530

    created class based view

--- Mainf/newcv/views.py
+++ Mainf/newcv/views.py
@@ -1,20 +1,33 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import View
 from .cvforms import CVFORM
 from .models import CVINFO
 # Create your views here.
-def Newcv(request):
-    if request.method == 'POST':
-        form = CVFORM(request.POST)
-        # check validation
-        if form.is_valid():
-            skills = form.cleaned_data["email_info"]
+#function based views
+# def Newcv(request):
+#     if request.method == 'POST':
+#         form = CVFORM(request.POST)
+#         # check validation
+#         if form.is_valid():
+#             email = form.cleaned_data["email_info"]
+#             return HttpResponse(email)
+#     else:
+#         form = CVFORM()
+#     return render(request, "new_cv_form.html", {'form' : form})    
 
-            return HttpResponse(skills)
-    else:
+#class based views
+class Newcv(View):
+    def get(self, request):
         form = CVFORM()
-    return render(request, "new_cv_form.html", {'form' : form})    
-
+        return render(request, "new_cv_form.html", {'form':form})
+    
+    def post(self, request):
+        form = CVFORM(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email_info"]
+            return HttpResponse(email)
+        
 
 
         
